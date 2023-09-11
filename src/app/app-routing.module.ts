19f98d1ad74f75e3df2132d257e8bc7170ed9cf1
petoGriac <peter.griac@garwan.sk>
2023-09-11 13:38:57 +0200

    homepage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListingComponent } from './pages/listing/listing.component';
 
 const routes: Routes = [
-  {path: '',  loadChildren: () => import('./pages/home/home-routes').then(r => r.HOME_ROUTES)},
   {path: 'l/:listDelay', component: ListingComponent},
   {path: 'd',  loadChildren: () => import('./pages/detail/detail-routes').then(r => r.DETAIL_ROUTES)},
+  {path: '',  loadChildren: () => import('./pages/home/home-routes').then(r => r.HOME_ROUTES)},
 ];
 
 @NgModule({
